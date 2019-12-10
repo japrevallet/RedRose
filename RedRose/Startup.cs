c55f22ee7c16b7d27aedd81352f39c84ba15496b
Jesse Prevallet <japrevallet@gmail.com>
2019-12-09 22:56:47 -0700

    Add project files.

---
+++ RedRose/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RedRose.Startup))]
+namespace RedRose
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
